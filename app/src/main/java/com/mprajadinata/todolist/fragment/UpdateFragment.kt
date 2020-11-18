49129da1f55314463fde3f0349787573dcc1609c
Praja510 <moh.prajadinata@gmail.com>
2020-11-18 11:40:32 +0700

    Initial commit

---
+++ app/src/main/java/com/mprajadinata/todolist/fragment/UpdateFragment.kt
@@ -0,0 +1,25 @@
+package com.mprajadinata.todolist.fragment
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.mprajadinata.todolist.R
+
+class UpdateFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_update, container, false)
+
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
+    }
+}
